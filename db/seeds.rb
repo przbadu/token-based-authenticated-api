0685d525b6ca6fd24d8a2b87a1fb31d3266c393d
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-03-03 18:27:56 +0545

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 User.create(email: 'test@example.com', password: 'password')
+
+
+Post.create!(title: 'post 1', description: 'small description')
+Post.create!(title: 'post 2', description: 'small description')
